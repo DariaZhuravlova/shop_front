3ad10412dc7dfa1be95f05b6931ccf0bc19268ff
DariaZhur <dariazhur89@gmail.com>
2024-05-02 11:19:35 +0300

    add vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Hello</h1>
+    <v-btn>
+      Button
+    </v-btn>
+    <v-list lines="one">
+      <v-list-item v-for="n in 3" :key="n" :title="'Item ' + n"
+        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"></v-list-item>
+    </v-list>
   </div>
 
 </template>
