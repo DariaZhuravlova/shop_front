7ff90429d843ed51fbb1b8c2342d644d6ad083ad
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-05-20 13:42:24 +0300

    added vee validate

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,40 @@
 <template>
   <v-container>
-    <v-form validate-on="submit lazy"  @submit.prevent="submit">
-    <v-row justify="center">
-      <v-col cols="12" md="6">
-        <v-text-field
-         :rules="rules"
-          variant="solo"
-          v-model="productName"
-          label="Product Name"
-        ></v-text-field>
-        <v-text-field
-         :rules="rules"
-          variant="solo"
-          v-model="productPrice"
-          label="Product Price"
-          type="number"
-          min="0"
-        ></v-text-field>
-        <v-btn
-        :loading="loading"
-        class="mt-2"
-        text="Submit"
-        type="submit"
-        block
-      ></v-btn>
-      </v-col>
-    </v-row>
-  </v-form>
+    <v-form @submit.prevent="submit">
+      <v-row justify="center">
+        <v-col cols="12" md="6">
+          <v-text-field
+            variant="solo"
+            v-model="productName.value.value"
+            :error-messages="productName.errorMessage.value"
+            label="Product Name"
+          ></v-text-field>
+          <v-text-field
+            variant="solo"
+            v-model="productPrice.value.value"
+            :error-messages="productPrice.errorMessage.value"
+            label="Product Price"
+            type="number"
+            min="0"
+          ></v-text-field>
+          <v-btn class="mt-2" text="Submit" type="submit" block></v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
 
     <v-row>
-      <v-col v-for="product in productStore.products" :key="product._id" cols="12" md="4">
+      <v-col
+        v-for="product in productStore.products"
+        :key="product._id"
+        cols="12"
+        md="4"
+      >
         <v-card class="product-card">
           <v-card-title>{{ product.name }}</v-card-title>
           <p>Price: ${{ product.price }}</p>
-          <v-btn color="error" @click="deleteProduct(product._id)">Delete</v-btn>
+          <v-btn color="error" @click="deleteProduct(product._id)"
+            >Delete</v-btn
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -43,46 +44,68 @@
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import { useProductStore } from "../stores/ProductStore";
-
+import { useField, useForm } from "vee-validate";
 const productStore = useProductStore();
 
 onMounted(() => {
   productStore.getProducts();
 });
 
-const productName = ref("");
-const productPrice = ref(0);
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: {
+    productName(value: string) {
+      if (value?.length >= 2) return true;
 
+      return "Имя содержит минимум 2 символа";
+    },
+    productPrice(value: number) {
+      if (value > 0) return true;
+
+      return "Введите цену";
+    },
+  },
+});
+
+const productName = useField("productName");
+const productPrice = useField("productPrice");
+
+const submit = handleSubmit(async (values: any) => {
+  await productStore.postProduct(
+    productName.value.value,
+    productPrice.value.value
+  );
+  console.log(values.productPrice, values.productName);
+  handleReset();
+});
 const deleteProduct = (productId: string) => {
   productStore.deleteProduct(productId);
 };
 
-const loading = ref(false)
-const userName = ref('')
+// const loading = ref(false);
+// const userName = ref("");
 
-const validation = (userName: string) => { 
-      if (!userName) return 'Please enter a user name.'
-      return true
-}
+// const validation = (userName: string) => {
+//   if (!userName) return "Please enter a user name.";
+//   return true;
+// };
 
-const rules = [(value: string) => validation(value)]
-const isFormValid = () => {
-//  return productName.value? true : false
-  return rules.every((rule) => {
-    const result = rule(productName.value)
-    console.log("result", result)
-    return typeof result !== 'string'
-  })
-}
-const submit = async (event: SubmitEvent) => {
-  console.log(isFormValid())
-  if (!isFormValid()) return
-  loading.value = true
-  await productStore.postProduct(productName.value, productPrice.value);
-  productName.value = "";
-  productPrice.value = 0;
-  loading.value = false
-}
+// const rules = [(value: string) => validation(value)];
+// const isFormValid = () => {
+//   return rules.every((rule) => {
+//     const result = rule(productName.value);
+//     console.log("result", result);
+//     return typeof result !== "string";
+//   });
+// };
+// const submit = async (event: SubmitEvent) => {
+//   console.log(isFormValid());
+//   if (!isFormValid()) return;
+//   loading.value = true;
+//   await productStore.postProduct(productName.value, productPrice.value);
+//   productName.value = "";
+//   productPrice.value = 0;
+//   loading.value = false;
+// };
 </script>
 
 <style scoped lang="scss">
@@ -93,7 +116,7 @@ const submit = async (event: SubmitEvent) => {
   text-align: center;
   padding: 20px;
 
-    .v-card-title {
+  .v-card-title {
     font-size: 18px;
     font-weight: bold;
     margin-bottom: 10px;
@@ -103,7 +126,6 @@ const submit = async (event: SubmitEvent) => {
     margin-top: 20px;
   }
 }
-
 </style>
 
 
