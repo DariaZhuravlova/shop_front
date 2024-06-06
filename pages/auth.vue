345c2d9b3189206de9e58b4f8643352623d625f7
DariaZhur <dariazhur89@gmail.com>
2024-06-06 11:21:43 +0300

    add tabs

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,39 +1,56 @@
 <template>
   <v-container>
-    <h2>Регистрация</h2>
-    <v-form @submit.prevent="submit">
-      <v-row justify="center">
-        <v-col cols="12" md="6">
-          <v-text-field
-            variant="solo"
-            v-model="username.value.value"
-            :error-messages="username.errorMessage.value"
-            label="User Name"
-          ></v-text-field>
-          <v-text-field
-            variant="solo"
-            v-model="password.value.value"
-            :error-messages="password.errorMessage.value"
-            label="password"
-            type="password"
-            min="0"
-          ></v-text-field>
-          <v-btn class="mt-2" text="Submit" type="submit" block></v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
+    <v-card>
+      <v-tabs v-model="tab" align-tabs="center" color="deep-purple-accent-4">
+        <v-tab :value="1">Login</v-tab>
+        <v-tab :value="2">Registration</v-tab>
+
+      </v-tabs>
+
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item :key="1" :value="1">
+          <v-container fluid>
+            <v-form @submit.prevent="submitRegister">
+              <v-row justify="center">
+                <v-col cols="12" md="6">
+                  <v-text-field variant="solo" v-model="username.value.value"
+                    :error-messages="username.errorMessage.value" label="User Name"></v-text-field>
+                  <v-text-field variant="solo" v-model="password.value.value"
+                    :error-messages="password.errorMessage.value" label="password" type="password"
+                    min="0"></v-text-field>
+                  <v-btn class="mt-2" text="Submit" type="submit" block></v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-tabs-window-item>
+        <v-tabs-window-item :key="2" :value="2">
+          <v-container fluid>
+
+            <v-form @submit.prevent="submitLogin">
+              <v-row justify="center">
+                <v-col cols="12" md="6">
+                  <v-text-field variant="solo" v-model="username.value.value"
+                    :error-messages="username.errorMessage.value" label="User Name"></v-text-field>
+                  <v-text-field variant="solo" v-model="password.value.value"
+                    :error-messages="password.errorMessage.value" label="password" type="password"
+                    min="0"></v-text-field>
+                  <v-btn class="mt-2" text="Submit" type="submit" block></v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card>
+
 
     <h2>Все пользователи</h2>
     <v-row>
-      <v-col
-        v-for="user in appStore.users"
-        :key="user._id"
-        cols="12"
-        md="4"
-      >
-        <v-card >
+      <v-col v-for="user in appStore.users" :key="user._id" cols="12" md="4">
+        <v-card>
           <v-card-title>{{ user.username }}</v-card-title>
-      
+
         </v-card>
       </v-col>
     </v-row>
@@ -46,7 +63,7 @@ import { ref, onMounted } from "vue";
 import { useAppStore } from "../stores/AppStore";
 import { useField, useForm } from "vee-validate";
 const appStore = useAppStore();
-
+const tab = ref(null);
 onMounted(() => {
   // appStore.checkAuth();
   appStore.getUsers();
@@ -71,16 +88,28 @@ const { handleSubmit, handleReset } = useForm({
 const username = useField("username");
 const password = useField("password");
 
-const submit = handleSubmit(async (values) => {
+const submitRegister = handleSubmit(async (values) => {
   const { username, password } = values;
 
   if (!username || !password) {
-    return alert("Заполните все поля"); 
+    return alert("Заполните все поля");
   }
 
   await appStore.register({ username, password });
   handleReset();
   appStore.getUsers();
+});
+
+
+const submitLogin = handleSubmit(async (values) => {
+  const { username, password } = values;
+
+  if (!username || !password) {
+    return alert("Заполните все поля");
+  }
+
+  await appStore.login({ username, password });
+  handleReset();
 });
 
 
@@ -92,6 +121,7 @@ h2 {
   margin-top: 40px;
   margin-bottom: 20px;
 }
+
 .product-card {
   margin: 20px;
   border-radius: 8px;
