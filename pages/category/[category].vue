e8759212651f6965c22a5fa751da721dae8676d6
DariaZhur <dariazhur89@gmail.com>
2024-07-08 11:34:57 +0300

    add category component

---
+++ pages/category/[category].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ route.params.category }}</h1>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+
+</script>
