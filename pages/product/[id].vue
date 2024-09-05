db7acae3ffa653c8a5345fd707694e4ca452c6e4
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-09-05 17:12:11 +0300

    added slider

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -4,7 +4,7 @@ import { productMenu } from '@/data/default/productMenu';
 import { replaceSpace } from '../utils';
 import { useProductStore } from '@/stores/ProductStore';
 import { useAppStore } from '@/stores/AppStore';
-
+import { ref } from 'vue';
 const route = useRoute();
 const router = useRouter();
 const productStore = useProductStore();
@@ -16,6 +16,7 @@ async function fetchProduct() {
     console.log(id);
 
     await productStore.getProduct(id);
+    console.log(productStore.currentProduct.image);
   } catch (e) {
     console.error(e);
     // router.push('/');
@@ -24,3 +25,28 @@ async function fetchProduct() {
 
 fetchProduct();
 </script>
+
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <h1 class="text-center mb-4">{{ productStore.currentProduct.name }}</h1>
+        <v-carousel
+          height="400"
+          show-arrows="hover"
+          cycle
+          hide-delimiter-background
+          v-if="productStore.currentProduct.image"
+        >
+          <v-carousel-item
+            v-for="img in productStore.currentProduct.image"
+            :key="img"
+            :src="'http://localhost:3001/' + img"
+          >
+          </v-carousel-item>
+        </v-carousel>
+        <atom-DiscountedPrice :price="productStore.currentProduct.price" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
