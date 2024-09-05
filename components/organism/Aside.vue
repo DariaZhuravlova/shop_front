9cfd4082bfe2b0f0eb1f587680bc38a3b3300106
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-09-05 20:50:00 +0300

    fix

--- components/organism/Aside.vue
+++ components/organism/Aside.vue
@@ -1,3 +1,10 @@
 <template>
-  <molecule-ProductCatalog />
+  <v-navigation-drawer
+    class="d-none d-sm-block"
+    location="left"
+    expand-on-hover
+    rail
+  >
+    <molecule-ProductCatalog />
+  </v-navigation-drawer>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,14 @@
 <template>
   <organism-Header>
     <v-row class="d-flex justify-center" v-if="$vuetify.display.mdAndUp">
-      <v-col cols="5" md="3" lg="3" xl="3">
-        <organism-Aside />
-      </v-col>
+      <organism-Aside />
       <v-col cols="7" md="9" lg="9" xl="9">
         <slot />
       </v-col>
     </v-row>
   </organism-Header>
 </template>
-
-<script setup>
+  
+  <script setup>
 import Header from '~/components/organism/Header.vue';
 </script>
