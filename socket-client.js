fc201206f6ef64351effd4bb2382cdd3132fc3df
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-10-31 11:23:24 +0200

    fix env

--- socket-client.js
+++ socket-client.js
@@ -1,21 +1,22 @@
 // client.js
 import { io } from 'socket.io-client';
+const envConfig = useNuxtApp().$envConfig;
 
-const socket = io('http://localhost:3002'); // Адрес вашего сервера
+const socket = io(`${envConfig.apiUrl}`); // Адрес вашего сервера
 
 // Функция для отправки сообщений на сервер
 export function sendMessage(message) {
-  socket.emit('message', message);
+    socket.emit('message', message);
 }
 
 // Слушаем сообщения от сервера
 socket.on('message', (msg) => {
-  console.log('Сообщение от сервера:', msg);
+    console.log('Сообщение от сервера:', msg);
 });
 
 console.log('Соединение с socket сервером установлено');
 
-console.log('coockie: ', document.cookie);
+// console.log('coockie: ', document.cookie);
 
 
 
