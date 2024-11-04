3de04902ab091d5bdd9e0ea0cbd174a06ffdc337
DariaZhur <dariazhur89@gmail.com>
2024-11-04 11:10:44 +0200

    change https

--- socket-client.js
+++ socket-client.js
@@ -1,28 +1,28 @@
 // client.js
 import { io } from 'socket.io-client';
 const apiUrl =
-    process.env.NODE_ENV === 'production'
-        ? 'wss://shop-back-mh7t.onrender.com'
-        : 'ws://localhost:3001';
+  process.env.NODE_ENV === 'production'
+    ? 'https://shop-back-mh7t.onrender.com'
+    : 'http://localhost:3001';
 
 const socket = io(apiUrl, {
-    transports: ['websocket'] // Явно указываем транспорт
+  transports: ['websocket'],
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
 });
 
 // Функция для отправки сообщений на сервер
 export function sendMessage(message) {
-    socket.emit('message', message);
+  socket.emit('message', message);
 }
 
 // Слушаем сообщения от сервера
 socket.on('message', (msg) => {
-    console.log('Сообщение от сервера:', msg);
+  console.log('Сообщение от сервера:', msg);
 });
 
 console.log('Соединение с socket сервером установлено');
 
 // console.log('coockie: ', document.cookie);
-
-
 
 export default socket;
