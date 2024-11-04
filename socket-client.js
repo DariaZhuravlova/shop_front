2d0e71a884c80831a002fe5700cfe5a459b0eeb7
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-11-04 11:18:56 +0200

    added connect

--- socket-client.js
+++ socket-client.js
@@ -1,27 +1,33 @@
 // client.js
 import { io } from 'socket.io-client';
 const apiUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://shop-back-mh7t.onrender.com'
-    : 'http://localhost:3001';
+    process.env.NODE_ENV === 'production'
+        ? 'https://shop-back-mh7t.onrender.com'
+        : 'http://localhost:3001';
 
 const socket = io(apiUrl, {
-  transports: ['websocket'],
-  reconnectionAttempts: 5,
-  reconnectionDelay: 2000,
+    transports: ['websocket'],
+    reconnectionAttempts: 5,
+    reconnectionDelay: 2000,
 });
 
 // Функция для отправки сообщений на сервер
 export function sendMessage(message) {
-  socket.emit('message', message);
+    socket.emit('message', message);
 }
 
 // Слушаем сообщения от сервера
 socket.on('message', (msg) => {
-  console.log('Сообщение от сервера:', msg);
+    console.log('Сообщение от сервера:', msg);
 });
 
-console.log('Соединение с socket сервером установлено');
+
+// Логируем успешное подключение
+socket.on('connect', () => {
+    console.log('Соединение с socket сервером установлено');
+});
+
+// console.log('Соединение с socket сервером установлено');
 
 // console.log('coockie: ', document.cookie);
 
