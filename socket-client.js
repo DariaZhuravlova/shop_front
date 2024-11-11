f0be8b56396ce2ae143b5d62031ed4ecf4eb6f4e
DariaZhur <dariazhur89@gmail.com>
2024-11-11 10:39:30 +0200

    fix

--- socket-client.js
+++ socket-client.js
@@ -2,19 +2,43 @@
 import  io  from 'socket.io-client';
 const apiUrl =
     process.env.NODE_ENV === 'production'
-        ? 'https://shop-back-mh7t.onrender.com:3001'
+        ? 'https://shop-back-mh7t.onrender.com'
         : 'http://localhost:3001';
 
+console.log('apiUrl: ', apiUrl);
+        
 const socket = io.connect(apiUrl)
+
+// const socket = io.connect('https://shop-back-mh7t.onrender.com:3001')
+
+// const socket2 = io.connect('https://shop-back-mh7t.onrender.com')
 
 // Функция для отправки сообщений на сервер
 export function sendMessage(message) {
     socket.emit('message', message);
 }
+socket.on('connect', () => {
+    console.log('Socket connected:', socket.id);
+});
+
+// socket2.on('connect', () => {
+//     console.log('Socket2 connected:', socket2.id);
+// });
+
 
 // Слушаем сообщения от сервера
 socket.on('message', (msg) => {
     console.log('Сообщение от сервера:', msg);
+});
+
+console.log('socket: ', socket);
+
+socket.on('connect_error', (error) => {
+    console.error('Connection error:', error);
+});
+
+socket.on('reconnect_attempt', () => {
+    console.log('Reconnecting...');
 });
 
 
