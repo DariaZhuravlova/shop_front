02405a5026254af6ff0580c1db1f25f8b852f51e
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-06 19:40:40 +0200

    added table for all chats

--- components/organism/AdminMessages.vue
+++ components/organism/AdminMessages.vue
@@ -9,6 +9,15 @@ const apiUrl = useRuntimeConfig().public.apiUrl;
 
 const socket = io(apiUrl);
 initSocketEvents(socket);
+
+function toggleChat(data) {
+  appStore.isOpenChat = true;
+  appStore.selectedChatUser = data;
+  appStore.allChatMessages = [];
+
+  socket.emit('getAllMsgsKick', data);
+}
+
 onMounted(() => {
   getMsgsListKick(socket);
 });
@@ -17,7 +26,30 @@ onMounted(() => {
 
 
 <template>
-  <div class="">
-    <h1>Сщщбщения</h1>
+  <div class="orders-admin">
+    <h1 class="title">Сообщения</h1>
+    <v-table class="order-table" height="300px" fixed-header>
+      <thead>
+        <tr>
+          <th class="text-left">Имя Клиента</th>
+          <th class="text-left">Телефон</th>
+          <th class="text-left">E-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, ind) in appStore.msgsList"
+          :key="ind"
+          class="order-row"
+          @click="toggleChat(item)"
+        >
+          <td>rrrr</td>
+          <td v-if="item.phone">{{ item.phone }}</td>
+          <td v-else>{{ item.fingerPrint }}</td>
+
+          <td>rrrrr</td>
+        </tr>
+      </tbody>
+    </v-table>
   </div>
 </template>
